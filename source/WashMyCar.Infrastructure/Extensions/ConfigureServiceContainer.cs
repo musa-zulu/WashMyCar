cf0fd73bf208f40324c4d061204d78e9638e11e1
Musa <zuluchs@gmail.com>
2022-06-02 10:32:19 +0200

    Solutions
    Added web and infrastructure projects

---
+++ source/WashMyCar.Infrastructure/Extensions/ConfigureServiceContainer.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using WashMyCar.Infrastructure.Mapping;
+
+namespace WashMyCar.Infrastructure.Extensions
+{
+    public static class ConfigureServiceContainer
+    {
+        public static void AddAutoMapper(this IServiceCollection serviceCollection)
+        {
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new ColorProfile());
+                
+            });
+            IMapper mapper = mappingConfig.CreateMapper();
+            serviceCollection.AddSingleton(mapper);
+        }
+
+    }
+}
---
+++ source/WashMyCar.Infrastructure/Mapping/ColorProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using WashMyCar.Core.Domain;
+using WashMyCar.Core.Request;
+using WashMyCar.Core.Response;
+
+namespace WashMyCar.Infrastructure.Mapping
+{
+    public class ColorProfile : Profile
+    {
+        public ColorProfile()
+        {
+            CreateMap<ColorRequest, Color>().ReverseMap();                
+            CreateMap<ColorResponse, Color>().ReverseMap();                
+            CreateMap<List<ColorResponse>, List<Color>>().ReverseMap();                
+        }
+    }
+}
